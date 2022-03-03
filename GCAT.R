feaec136db948e080ad1b2e321ac718726afa33e
Bioinformatic Research Group in Epidemiology (BRGE) <juanr.gonzalez@isglobal.org>
2022-03-03 10:54:30 +0100

    Update GCAT.R

--- GCAT.R
+++ GCAT.R
@@ -10,7 +10,7 @@ library('dsOmicsClient')
 # set the connection with Opal
 #
 builder <- DSI::newDSLoginBuilder()
-builder$append(server = "gcat", url = "https://datashield.isglobal.org",
+builder$append(server = "gcat", url = "https://datashield.isglobal.org/repo",
                user =  "invited", password = "123456",
                profile = "rock-inma")
 logindata <- builder$build()
@@ -26,13 +26,6 @@ DSI::datashield.assign.expr(conns = conns, symbol = "geno",
 DSI::datashield.assign.table(conns, "pheno", "GCAT.GCATCoreSpain_V2_phenotypes_treated")
 
 
-# Some descriptive analyses
-
-ds.genoDimensions('geno')
-
-pca <- ds.PCASNPS('geno')
-plotPCASNPS(pca)
-ds.scatterPlot('pheno$PC1', 'pheno$PC2', datasources = conns)
 
 # Put genotype and phenotypes in a single object (Genomic Data Storage)
 
@@ -44,6 +37,16 @@ ds.genoDimensions('gds.object')
 
 ds.getChromosomeNames('gds.object')
 ds.varLabels('gds.object')
+
+# Some descriptive analyses
+
+ds.genoDimensions('geno')
+
+pca <- ds.PCASNPS('geno')
+plotPCASNPS(pca, group = 'gds.object$CMBD_ORIGIN')
+ds.scatterPlot('pheno$PC1', 'pheno$PC2', datasources = conns)
+
+
 
 # Test HWE
 
@@ -59,28 +62,37 @@ freq <- ds.alleleFrequency('gds.object')
 
 # 2721 Pure hyperglyceridemia
 # 7330 Age-related osteoporosis without current pathological fracture
+# 4019 Essential hypertension
 
 ds.table('pheno$V_4019')
 
 # GWAS
-
-ans <- ds.metaGWAS('gds.object', 'V_4019 ~  PC1 + PC2 + PC3 + PC4 + SEX')
-manhattan(ans$gcat)
-qqplot(ans$gcat$p.value)
-LocusZoom(ans$gcat)
+ans1 <- ds.fastGWAS('gds.object', 'V_4019 ~  PC1 + PC2 + PC3 + PC4 + SEX')
+ans2 <- ds.metaGWAS('gds.object', 'V_4019 ~  PC1 + PC2 + PC3 + PC4 + SEX')
+manhattan(ans2$gcat)
+qqplot(ans2$gcat$p.value)
+LocusZoom(ans2$gcat)
 
 #
 # Polygenic risk score: https://www.pgscatalog.org/
 #
 
-# HDL cholesterol
-ds.PRS(resources = 'resource', pgs_id = "PGS000660", 
-       table = 'pheno', table_id_column = "EGA_ID")
 
 ds.colnames('pheno')
 
-ds.glm('V_4019 ~ SEX', data='pheno', family="gaussian")
+# HDL cholesterol
+ds.PRS(resources = 'geno', pgs_id = "PGS000660", 
+       table = 'pheno', table_id_column = "EGA_ID")
 
-datashield.logout(conns)
+# Essential hypertension
+ds.PRS(resources = 'geno', pgs_id = "PGS000958", 
+       table = 'pheno', table_id_column = "EGA_ID")
 
+
+
+ds.colnames('pheno')
+
+ds.glm('V_4019 ~ SEX + prs_PGS000660', data='pheno', family="binomial")
+
+ds.glm('V_4019 ~ SEX + prs_PGS000958', data='pheno', family="binomial")
 
